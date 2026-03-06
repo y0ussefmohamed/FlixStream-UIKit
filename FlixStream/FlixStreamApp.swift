016e2f6a1107633e436627af0fc68da5dd0021e7
Youssef Mohamed <youssef.mohamed.ib@gmail.com>
2026-03-06 19:11:53 +0200

    Initial Commit

---
+++ FlixStream/FlixStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlixStreamApp.swift
+//  FlixStream
+//
+//  Created by Youssef Mohamed on 06/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct FlixStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlixStreamTests/FlixStreamTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlixStreamTests.swift
+//  FlixStreamTests
+//
+//  Created by Youssef Mohamed on 06/03/2026.
+//
+
+import Testing
+@testable import FlixStream
+
+struct FlixStreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
